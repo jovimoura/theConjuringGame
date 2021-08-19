c3558ec287043cbf31d7f96e64c3265eea831183
jovimoura <joaovictormoura@poli.ufrj.br>
2021-08-18 22:56:42 -0300

    add ultimas mudancas jhonny versao final jhonny

--- code.js
+++ code.js
@@ -1,19 +1,24 @@
 function trocaPagina(pagina){
+    
     location.replace(pagina);
 }
 
 function avancaPagina(proximaPagina,respostaCorreta){
     while(true){
         let escolha = prompt("Escolha a opção A ou B").toLocaleLowerCase();
-        if(escolha === "a" || escolha === "b"){
+        if(escolha === 'a' || escolha === 'b'){
             if(escolha === respostaCorreta){
                 trocaPagina(proximaPagina);
+                
             }else{
-                trocaPagina("gameOver.html");
+                trocaPagina("../gameOver/gameOver.html");
+                
             }
+            break
         }else{
             alert("Resposta Inválida!");
         }
-        break;
+        
+        
     }
 }
